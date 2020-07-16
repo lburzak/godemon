2ee73bfbc83dcd7028c4602bd55f63203f9da7f7
polydome <polydome@outlook.com>
2020-07-16 19:39:39 +0200

    Improve GetChallengeStatusUseCase error handling

--- domain/src/main/java/com/polydome/godemon/domain/usecase/GetChallengeStatusUseCase.java
+++ domain/src/main/java/com/polydome/godemon/domain/usecase/GetChallengeStatusUseCase.java
@@ -2,10 +2,13 @@ package com.polydome.godemon.domain.usecase;
 
 import com.polydome.godemon.domain.entity.Challenge;
 import com.polydome.godemon.domain.entity.ChallengeStage;
-import com.polydome.godemon.domain.entity.Challenger;
+import com.polydome.godemon.domain.exception.ActionForbiddenException;
+import com.polydome.godemon.domain.exception.AuthenticationException;
+import com.polydome.godemon.domain.exception.NoSuchChallengeException;
 import com.polydome.godemon.domain.model.ChallengeStatus;
 import com.polydome.godemon.domain.repository.ChallengeRepository;
 import com.polydome.godemon.domain.repository.ChallengerRepository;
+import com.polydome.godemon.domain.repository.exception.NoSuchEntityException;
 import com.polydome.godemon.domain.service.ChallengeService;
 import lombok.AllArgsConstructor;
 
@@ -15,10 +18,26 @@ public class GetChallengeStatusUseCase {
     private final ChallengeRepository challengeRepository;
     private final ChallengeService challengeService;
 
-    public ChallengeStatus execute(long discordId, int challengeId) {
-        Challenger challenger = challengerRepository.findByDiscordId(discordId);
-        Challenge challenge = challengeRepository.findChallenge(challengeId);
-        challenge = challengeService.synchronizeChallenge(challenge.getId());
+    public ChallengeStatus execute(long challengerId, int challengeId) throws ActionForbiddenException, AuthenticationException, NoSuchChallengeException {
+        try {
+            challengerRepository.findByDiscordId(challengerId);
+        } catch (NoSuchEntityException e) {
+            throw new AuthenticationException(String.format("Challenger[%d] not registered", challengerId));
+        }
+
+        Challenge challenge;
+
+        try {
+            challenge = challengeRepository.findChallenge(challengeId);
+        } catch (NoSuchEntityException e) {
+            throw new NoSuchChallengeException(challengeId);
+        }
+
+        if (challenge.getParticipants().stream().noneMatch(challenger -> challenger.getId() == challengerId))
+            throw new ActionForbiddenException(String.format("Challenger[%d] does not participate in Challenge[%d]", challengerId, challengeId));
+
+        challengeService.synchronizeChallenge(challengeId);
+        challenge = challengeRepository.findChallenge(challengeId);
 
         return ChallengeStatus.builder()
                 .ended(challenge.getStatus() == ChallengeStage.FAILED)
