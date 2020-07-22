ebbaa144bb60afe5616cc13c10da16ed85601479
polydome <polydome@outlook.com>
2020-07-22 12:14:51 +0200

    Fix switch control leak

--- discord-bot/src/main/java/com/polydome/godemon/discordbot/reaction/ReactionActionBus.java
+++ discord-bot/src/main/java/com/polydome/godemon/discordbot/reaction/ReactionActionBus.java
@@ -47,8 +47,12 @@ public class ReactionActionBus extends ListenerAdapter implements MessageActionR
         }
 
         switch (action) {
-            case CREATE_CHALLENGE: listener.onCreateChallenge(event);
-            case JOIN_CHALLENGE: listener.onJoinChallenge(event, argStorage.getIntArg(messageId, 0));
+            case CREATE_CHALLENGE:
+                listener.onCreateChallenge(event);
+                break;
+            case JOIN_CHALLENGE:
+                listener.onJoinChallenge(event, argStorage.getIntArg(messageId, 0));
+                break;
         }
     }
 
